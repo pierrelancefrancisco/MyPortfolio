c22ab4fb6888f163327074fbcec2502c430807c0
pierrelancefrancisco <pierrelance.francisco@benilde.edu.ph>
2023-11-24 14:30:01 +0800

    Added new navbar and content

--- scriptabout.js
+++ scriptabout.js
@@ -22,3 +22,18 @@ cardHolder
 
     current.textContent = current.textContent.includes('Read More...') ? 'Read Less...' : 'ReadMore...';
 })
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+ let i;
+ let slides = document.getElementsByClassName("card__img__slides");
+ for (i = 0; i < slides.length; i++) {
+   slides[i].style.opacity = 0;
+ }
+ slideIndex++;
+ if (slideIndex > slides.length) {slideIndex = 1}
+ slides[slideIndex-1].style.opacity = 1;
+ setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
